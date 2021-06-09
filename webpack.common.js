b0eeb0c2517aa82bac0de654b68058b1c31465a9
jugonzalez <jugonzalez@hugeinc.com>
2021-06-08 21:21:56 -0500

    feat: minify files

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,3 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-
 module.exports = {
     entry: {
         main: './src/index.js',
@@ -8,19 +6,8 @@ module.exports = {
     output: {
         assetModuleFilename: 'images/[name].[hash].[ext]'
     },
-    plugins: [new HtmlWebpackPlugin({
-        template: './src/template.html'
-    })],
     module: {
         rules: [
-            {
-                test: /\.scss$/i,
-                use: [
-                    'style-loader', // 3. Injects styles into DOM
-                    'css-loader', // 2. Turns css into commonjs
-                    'sass-loader' // 1. Turns sass into css
-                ],
-            },
             {
                 test: /\.html$/i,
                 loader: 'html-loader',
